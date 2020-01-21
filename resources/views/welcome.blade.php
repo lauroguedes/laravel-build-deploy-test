50bf36505e4dfd657d64ac90d4069583c748fd41
Lauro Guedes <leowgweb@gmail.com>
2020-01-21 13:44:26 -0300

    update for 6.6 welcome file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 6.8
+                    Laravel 6.6
                 </div>
 
                 <div class="links">
