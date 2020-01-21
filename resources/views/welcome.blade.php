ecf2120b2ad5e51e3f8c21f04713a1379de5f455
Lauro Guedes <leowgweb@gmail.com>
2020-01-21 10:49:30 -0300

    update file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 6
+                    Laravel 6.5
                 </div>
 
                 <div class="links">
